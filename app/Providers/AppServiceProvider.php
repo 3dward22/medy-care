b5773abb7acda58f88bda3e3c89f8027fd763ca0
3dward22 <159800005+3dward22@users.noreply.github.com>
2026-02-12 20:48:53 +0800

    hoping

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Foundation\MaintenanceMode;
 use Illuminate\Foundation\FileBasedMaintenanceMode;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
